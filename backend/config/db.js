478a79fdee9e196495ae76612b28d01f83d6eaba
MateoBossa <mateobossa909@gmail.com>
2026-04-18 12:46:23 -0500

    cambio en archivo db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  host:     process.env.MYSQLHOST     || process.env.DB_HOST,
+  port:     process.env.MYSQLPORT     || process.env.DB_PORT     || 3306,
+  database: process.env.MYSQL_DATABASE|| process.env.DB_NAME,
+  user:     process.env.MYSQLUSER     || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASS,
   waitForConnections: true,
   connectionLimit: 10,
   ssl: process.env.DB_HOST !== 'localhost' ? { rejectUnauthorized: false } : false
