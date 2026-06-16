237e1faa862c168b814e15944a4052f1a96d8ba9
MateoBossa <mateobossa909@gmail.com>
2026-06-16 17:57:22 -0500

    add a new origin in index.js v2

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,16 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5500',
   'http://localhost:4200',
+  'http://127.0.0.1:4200',
   'http://127.0.0.1:5500',
   'https://kanekong909.github.io'
 ].filter(Boolean);
 
 app.use(cors({
   origin: (origin, callback) => {
+    // ESTA LÍNEA TE DIRÁ EN CONSOLA QUÉ URL EXACTA ESTÁ LLEGANDO:
+    console.log("CORS Origin recibido:", origin); 
+
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
